1b8c8e7b21e9c2a0fe6c8b517eebab759fc7da03
andikan <andikan.io@gmail.com>
2014-04-27 16:00:02 +0800

    change layout

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,7 +1,13 @@
 class Api::HomeController < ApplicationController
+  before_filter :check_format
   respond_to :json
   
   def ping
     @current_time = Time.now
+  end
+
+  private
+  def check_format
+    render :json => {error: "invalid format"}, :status => 406 unless params[:format] == 'json'
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :check_format
-  def check_format
-    render :json => {error: "invalid format"}, :status => 406 unless params[:format] == 'json'
-  end
 end
